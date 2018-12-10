45bae2ea5eb79bfb9eda636344c214e7bf67cd61
Dmitry16 <europartner@inbox.ru>
2018-12-10 15:35:00 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.get('/get-transactions', async (req, res) => {
     const query = 'base=EUR';
     const { baseUrl } = require('./config');
     
-    data = await getCurrenciesRates(request, baseUrl, query);
+    let data = await getCurrenciesRates(request, baseUrl, query);
     
     let randomTransactions = generateRandomTransactions(baseUrl, query, data);
     
